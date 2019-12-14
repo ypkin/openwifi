269f85db67dd6ab623e19c78e36cbe8192dda865
wifimedia <dungtd8x@gmail.com>
2019-12-14 17:32:14 +0700

    fix lic

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -71,13 +71,7 @@ wa901nd() { #checking internet
 	fi
 }
 
-dnsctl=$(uci -q get meshdesk.internet1.dns)
-ip=`nslookup $dnsctl | grep 'Address' | grep -v '127.0.0.1' | grep -v '8.8.8.8' | grep -v '0.0.0.0'|grep -v '::' | awk '{print $3}'`
-if [ "$ip" != "" ] &&  [ -e /etc/config/meshdesk ];then
-	uci set meshdesk.internet1.ip=$ip
-	uci commit meshdesk
-fi
-}
+#ip=`nslookup $dnsctl | grep 'Address' | grep -v '127.0.0.1' | grep -v '8.8.8.8' | grep -v '0.0.0.0'|grep -v '::' | awk '{print $3}'`#
 
 checking (){
 	#model=$(cat /proc/cpuinfo | grep 'machine' | cut -f2 -d ":" | cut -b 10-50 | tr ' ' '_')
