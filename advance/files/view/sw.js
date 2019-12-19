b8b6ff0d5a364a025e154b978bf1b39c2527bf89
wifimedia <dungtd8x@gmail.com>
2019-12-19 17:47:44 +0700

    updte js

---
+++ advance/files/view/sw.js
@@ -0,0 +1,24 @@
+'use strict';
+'require fs';
+'require ui';
+'require uci';
+'require form';
+'require network';
+'require tools.widgets as widgets';
+
+return L.view.extend({
+
+	render: function() {
+		var m, s, sw;
+
+		m = new form.Map('wifimedia');
+
+		s = m.section(form.TypedSection, 'switchmode', _('Switch Mode'));
+		s.anonymous = true;
+		s.addremove = true;
+	
+		sw = s.option(form.Flag,"switch_port","All Port LAN & WAN")
+		sw.rmempty = false
+		return m.render();
+	}
+});
