f61d484a1da88eda88a097da520fb4c0399f145b
wifimedia <dungtd8x@gmail.com>
2019-12-20 18:55:38 +0700

    ...

--- wifimedia_portal/files/portal_model.lua
+++ wifimedia_portal/files/portal_model.lua
@@ -19,35 +19,64 @@ s.anonymous = true
 s.addremove = false
 --s:option( Value, "ndsname","Name")
 --s:option( Value, "nds_apkey","APKEY")
-s:tab("basic","Basic")
+s:tab("basic","General Setup")
 s:tab("advance","Advanced")
 s:tab("network","Network")
-s:taboption( "basic",Value, "domain","Captive portal url","portal.nextify.vn/splash")
+service = s:taboption( "basic",Flag, "enable_cpn","Captive Portal Sevice","On/Off")
+service.rmempty = false
+
+network = s:taboption( "basic",ListValue, "network","Interface")
+network:value("br-hotspot", "Hotspot")
+network:value("br-lan", "LAN")
+url = s:taboption( "basic",Value, "domain","Captive portal url","portal.nextify.vn/splash")
+--url.rmempty = true
+--url:depends({enable_cpn="1"})
+
 --s:taboption( "basic",Value, "redirecturl","Redirect URL","https://google.com.vn")
-s:taboption( "basic",Value, "preauthenticated_users","Walled Garden","google.com.vn, vnexpress.net")
-s:taboption( "advance",Value, "maxclients","Maxclients","Max Clients:250")
-s:taboption( "advance",Value, "preauthidletimeout","Preauthidletimeout","Default: > 30 Mins")
-s:taboption( "advance",Value, "authidletimeout","Authidletimeout","Default: > 120 Mins")
-s:taboption( "advance",Value, "sessiontimeout","Sessiontimeout","Default : 120 Mins")
-s:taboption( "advance",Value, "checkinterval","Checkinterval","Default: 10 Mins")
-s:taboption( "basic",Flag, "facebook","Bypass Facebook")
+pre = s:taboption( "basic",Value, "preauthenticated_users","Walled Garden","google.com.vn, vnexpress.net")
+--pre:depends({enable_cpn="1"})
+mac = s:taboption( "advance",Value, "maxclients","Maxclients","Max Clients:250")
+mac:depends({enable_cpn="1"})
+prau = s:taboption( "advance",Value, "preauthidletimeout","Preauthidletimeout","Default: > 30 Mins")
+prau:depends({enable_cpn="1"})
+auth = s:taboption( "advance",Value, "authidletimeout","Authidletimeout","Default: > 120 Mins")
+auth:depends({enable_cpn="1"})
+ses = s:taboption( "advance",Value, "sessiontimeout","Sessiontimeout","Default : 120 Mins")
+ses:depends({enable_cpn="1"})
+chk = s:taboption( "advance",Value, "checkinterval","Checkinterval","Default: 10 Mins")
+chk:depends({enable_cpn="1"})
+fb = s:taboption( "basic",Flag, "facebook","Bypass Facebook")
+fb:depends({enable_cpn="1"})
 --s:taboption( "basic",Flag, "https","Bypass https")
 dhcpextension = s:taboption( "basic",Flag, "dhcpextension","DHCP Extension")
 dhcpextension.rmempty = false
+dhcpextension:depends({enable_cpn="1"})
 
 cpn = s:taboption( "basic",Flag, "cpnurl","CPN Clients detect")
 cpn.rmempty = false
-
-network = s:taboption( "network",ListValue, "network","Interface")
-network:value("br-hotspot", "Hotspot")
-network:value("br-lan", "LAN")
-
+cpn:depends({enable_cpn="1"})
+--network:depends({enable_cpn="1"})
+--[[On/Off Service]]--
+function service.write(self, section, value)
+if value == self.enabled then
+		luci.sys.call("uci set nodogsplash.@nodogsplash[0].enabled='1' && uci commit nodogsplash")
+		luci.util.exec("echo '' >/etc/crontabs/roots")
+		luci.util.exec("crontab /etc/cron_nds -u nds && /etc/init.d/cron restart")
+		luci.util.exec("/etc/init.d/nodogsplash enable")
+	else
+		luci.sys.exec("uci set nodogsplash.@nodogsplash[0].enabled='0' && uci commit nodogsplash")
+		luci.util.exec("echo '* * * * * /sbin/wifimedia/controller.sh heartbeat' >/etc/crontabs/roots")
+		luci.util.exec("echo ''>/etc/crontabs/nds && /etc/init.d/cron restart")
+		luci.util.exec("/etc/init.d/nodogsplash disable && /etc/init.d/nodogsplash stop")	
+	end
+	return Flag.write(self, section, value)
+end
+		-- retain server list even if disabled
+function service.remove() end
 
 function cpn.write(self, section, value)
 if value == self.enabled then
 		luci.util.exec("crontab /etc/cron_nds -u nds && /etc/init.d/cron restart")
-	--else
-	--	luci.sys.call("echo '*/5 * * * * /sbin/wifimedia/captive_portal.sh heartbeat'>/etc/crontabs/nds && /etc/init.d/cron restart")
 	end
 	return Flag.write(self, section, value)
 end
@@ -58,18 +87,9 @@ function dhcpextension.write(self, section, value)
 if value == self.enabled then
 		luci.sys.call("uci set network.local='interface'")
 		luci.sys.call("uci set network.local.proto='relay'")
-		--luci.sys.call("uci set network.local.ipaddr='172.16.99.1'")
-		--luci.sys.call("uci add_list network.local.network='lan'")
-		--luci.sys.call("uci add_list network.local.network='wan'")
-		--luci.sys.call("uci set dhcp.lan.ignore='1' && uci commit dhcp ")
-		--luci.sys.call("uci set wireless.@wifi-iface[0].network='lan' && uci commit network")
-		--luci.sys.call("uci set nodogsplash.@nodogsplash[0].gatewayinterface='br-lan'")
 		luci.sys.call("uci commit")
 	else
 		luci.sys.call("uci del network.local")
-		--luci.sys.call("uci set dhcp.hotspot.ignore='0' && uci commit dhcp")
-		--luci.sys.call("uci set wireless.@wifi-iface[0].network='hotspot' && uci commit network")
-		--luci.sys.call("uci set nodogsplash.@nodogsplash[0].gatewayinterface='br-private'")
 		luci.sys.call("uci commit")
 	end
 	return Flag.write(self, section, value)
@@ -104,7 +124,7 @@ t = m:section(Table, captive_process_status())
 t.anonymous = true
 
 t:option(DummyValue, "status","Captive portal status")
-
+--[[
 	if nixio.fs.access("/etc/rc.d/S95nodogsplash") then
 	  disable = t:option(Button, "_disable","Disable Startup")
 	  disable.inputstyle = "remove"
@@ -130,5 +150,5 @@ t:option(DummyValue, "status","Captive portal status")
 			)			
 	  end
 	end
-
+]]--
 return m
