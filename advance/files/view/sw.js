3fe6f7436385914c446a057e39ff6101b9c3ea53
wifimedia <dungtd8x@gmail.com>
2019-12-21 22:26:06 +0700

    ...

--- advance/files/view/sw.js
+++ advance/files/view/sw.js
@@ -8,7 +8,7 @@ return L.view.extend({
 	load: function() {
 		return Promise.all([
 			uci.load('luci'),
-			uci.load('system')
+			uci.load('wifimedia')
 		]);
 	},
 
@@ -19,20 +19,21 @@ return L.view.extend({
 
 		s = m.section(form.TypedSection, 'switchmode', _('Switch Mode'));
 		s.anonymous = true;
-		s.addremove = true;
+		s.addremove = false;
 	
 		o = s.option(form.Flag,'switch_port',_('All Port LAN & WAN'));
 		o.rmempty = false;
+		o.ucisection = 'switch5port';
 		o.write = function(section_id, value) {
 			sw_enabled = +value;
 			if (!sw_enabled)
-			uci.set('wifimedia', '@switchmode[0]', 'switch_port','1')
+			uci.set('wifimedia', 'switch5port', 'switch_port','1');
 		};
-		o.load = function(section_id) {
-			return (sw_enabled == 1 &&
-				uci.get('wifimedia', '@switchmode[0]') != null &&
-				uci.get('wifimedia', '@switchmode[0]', 'switch_port') != 0) ? '1' : '0';
-			};
+		//o.load = function(section_id) {
+		//	return (sw_enabled == 1 &&
+		//		uci.get('wifimedia', '@switchmode[0]') != null &&
+		//		uci.get('wifimedia', '@switchmode[0]', 'switch_port') != 0) ? '1' : '0';
+		//	};
 
 		return m.render();
 	}
