95465d42a8d863d7b029ab60f2fe7453c2c16af9
wifimedia <dungtd8x@gmail.com>
2019-12-24 18:02:12 +0700

    ...

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -313,9 +313,13 @@ if [ "${curl_result}" -eq 0 ]; then
 				rm /etc/init.d/wifimedia_check
 				rm /etc/init.d/S30wifimedia_check
 				rm /etc/init.d/K105wifimedia_check
+				echo "" > /etc/crontabs/wificode
 				license_local
+			else
+				echo "0 0 * * * /sbin/wifimedia/controller.sh license_srv" > /etc/crontabs/wificode
 			fi
-		done	
+		done
+		/etc/init.d/cron restart
 	fi
 fi
 }
