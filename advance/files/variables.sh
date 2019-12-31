da1de1132e2ac25a0ecfe539dea7ec5f211efcc2
wifimedia <dungtd8x@gmail.com>
2019-12-31 16:37:49 +0700

    ...

--- advance/files/variables.sh
+++ advance/files/variables.sh
@@ -8,6 +8,10 @@ gateway=$(route -n | grep 'UG' | grep 'br-wan' | awk '{ print $2 }')
 licensekey=/tmp/upgrade/licensekey
 code_srv="http://firmware.wifimedia.com.vn/hardware_active"
 blacklist="http://firmware.wifimedia.com.vn/blacklist"
+# Defines the URL to check the firmware at
+url_fw="http://firmware.wifimedia.com.vn/tplink/$board_name.bin"
+url_v="http://firmware.wifimedia.com.vn/tplink/version"
+url_srv="http://firmware.wifimedia.com.vn/hardware"
 device=$(cat /sys/class/ieee80211/phy0/macaddress | sed 's/:/-/g' | tr a-z A-Z)
 apid=$(echo $device | sed 's/:/-/g')
 
@@ -17,15 +21,18 @@ global_device=`ifconfig eth0 | grep 'HWaddr' | awk '{ print $5 }'| tr a-z A-Z` #
 rssi_on=$(uci -q get wifimedia.@advance[0].enable)
 #---------------controller online----------------
 hardware=/tmp/upgrade/hardware
-url_srv="http://firmware.wifimedia.com.vn/hardware"
 version=/tmp/upgrade/version
-device_fw=$(cat /sys/class/ieee80211/phy0/macaddress |sed 's/:/-/g' | tr a-z A-Z)
-# Defines the URL to check the firmware at
-url_fw="http://firmware.wifimedia.com.vn/tplink/$board_name.bin"
-url_v="http://firmware.wifimedia.com.vn/tplink/version"
-response_file=/tmp/wifimedia
+response_file=/tmp/wifimedia_cfg
 touch $response_file
 hash256=$(sha256sum $response_file | awk '{print $1}')
+device_fw=$(cat /sys/class/ieee80211/phy0/macaddress |sed 's/:/-/g' | tr a-z A-Z)
+link_config=`uci -q get wifimedia.@sync[0].server`
+cpn_url=`uci -q get wifimedia.@nodogsplash[0].cpnurl`
+
+ip_wan=$(ifconfig br-wan | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')
+ip_lan=$(ifconfig br-lan | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')
+#ip_gateway=$(route -n | grep 'UG' | grep 'br-wan' | awk '{ print $2 }')
+
 #echo "Waiting a bit..."
 #sleep $(head -30 /dev/urandom | tr -dc "0123456789" | head -c1)
 if [ ! -d "/tmp/upgrade" ]; then mkdir /tmp/upgrade; fi
